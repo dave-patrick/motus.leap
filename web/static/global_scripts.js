13f847c4bf4f255fb2f7897039104cf06c5d1155
dave.patrick <davepatrick@gmail.com>
2026-07-27 10:16:10 -0700

    fix: convert View System Logs button to direct anchor tag and attach viewSystemLogs globally for SPA navigation

--- web/static/global_scripts.js
+++ web/static/global_scripts.js
@@ -88,3 +88,4 @@ async function authFetch(url, options = {}) {
 window.getCookie = getCookie;
 window.getAuthToken = getAuthToken;
 window.authFetch = authFetch;
+window.viewSystemLogs = function() { window.open('/system/logs', '_blank'); };
